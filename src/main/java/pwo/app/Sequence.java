444376b1974d53cfe82d6fe8c5b0abdf5beb19ec
dariuszknap1 <dariusz.knap1@pollub.edu.pl>
2022-10-09 19:41:26 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

---
+++ src/main/java/pwo/app/Sequence.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pwo.app;
+
+/**
+ *
+ * @author dariusz
+ */
+public class Sequence {
+
+    public static void main(String[] args) {
+        (new SeqToFileApp()).run(args);
+    }
+}
